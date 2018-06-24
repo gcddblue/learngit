f938bf69013be6611e850ce0d9e1a8b3cbc85277
gch <1024484268@qq.com>
2018-06-24 17:44:47 +0800

    add a new file

---
+++ test/js/test.js
@@ -0,0 +1,10 @@
+$(function(){
+	$(this).addClass("name").removeClass("")
+	$(".name").click(function(){
+
+	})
+	$(this).on("click", function(){
+		jQuery("name").addClass("hhh").siblings().removeClass("")
+		
+	})
+})
